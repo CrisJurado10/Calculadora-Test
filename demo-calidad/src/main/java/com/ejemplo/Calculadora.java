9f40c7c2cd063c7def14b2e314d21e98301c5dae
CrisJurado10 <129802232+CrisJurado10@users.noreply.github.com>
2025-06-03 23:09:05 -0500

    Update Calculadora.java

--- demo-calidad/src/main/java/com/ejemplo/Calculadora.java
+++ demo-calidad/src/main/java/com/ejemplo/Calculadora.java
@@ -4,17 +4,7 @@ public class Calculadora {
 
     public int suma(int a, int b) {
         return a + b;  // bien
-    }
-
-    public int division(int a, int b) {
-        return a / b;  // posible división por cero
-    }
-
-    public int multiplicar(int a, int b) {
-    final int resultado = a * b;
-    return resultado;
-}
-
+    
 
     public int metodoNoProbado(int x) {
         if (x > 0)
